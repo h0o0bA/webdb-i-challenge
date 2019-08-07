44e63051972f890a13f5c01184f9256a80a85dcc
Ihab Ali <ihab@live.com>
2019-08-07 05:07:44 -0500

    added get('/') and ('/:id') end points

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -6,7 +6,28 @@ const db = require("../data/dbConfig.js");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("<h2>hello world</h2>");
+  db("accounts")
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ message: "Could not retrieve the list of accounts" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .first()
+    .then(account => {
+      if (account) {
+        res.status(200).json(account);
+      } else {
+        res.status(404).json({ message: "Account not found" });
+      }
+    });
 });
 
 module.exports = router;
