3bde09b931e200dd6db43f19d8d288843f1fd82d
Ihab Ali <ihab@live.com>
2019-08-07 05:08:50 -0500

    added post and put end points

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -30,4 +30,43 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  if (accountIsValid(req.body)) {
+    db("accounts")
+      .insert(req.body, "id")
+      .then(([id]) => id)
+      .then(id => {
+        db("accounts")
+          .where({ id })
+          .first()
+          .then(account => {
+            res.status(201).json(account);
+          });
+      })
+      .catch(() => {
+        res.status(500).json({ message: "Could not add the account" });
+      });
+  } else {
+    res.status(400).json({
+      message: "Please provide name and budget of zero or more for the account"
+    });
+  }
+});
+
+router.put("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count) {
+        res.status(200).json({ message: `${count} record(s) updated` });
+      } else {
+        res.status(404).json({ message: "Account not found" });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Could not update the account" });
+    });
+});
+
 module.exports = router;
